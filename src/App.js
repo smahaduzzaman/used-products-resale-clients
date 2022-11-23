6a9978d780ae58918a710a243b648b958b2dfa54
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-11-24 01:46:18 +0600

    Second Commit For Update Project Setting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Product Resale website core functionality</h1>
+      <button className="btn">Button</button>
+      <button className="btn btn-primary">Button</button>
+      <button className="btn btn-secondary">Button</button>
+      <button className="btn btn-accent">Button</button>
+      <button className="btn btn-ghost">Button</button>
+      <button className="btn btn-link">Button</button>
     </div>
   );
 }
