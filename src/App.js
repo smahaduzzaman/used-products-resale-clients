6417d7267600e6cf0d063c297b5f428b0353503f
smahaduzzaman <smahaduzzaman96@gmail.com>
2022-11-23 20:44:24 +0600

    First Commit For Creating Product Resale Website

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Product Resale website core functionality</h1>
+    </div>
+  );
+}
+
+export default App;
